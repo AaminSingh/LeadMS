4e74709c387b04a48d3185a0d40d4aa9ecbff8b5
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-09-02 22:26:45 +0530

    updating the file

--- leadms-backend/src/server.js
+++ leadms-backend/src/server.js
@@ -32,6 +32,9 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
