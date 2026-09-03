dd6c6a48d3358fd0364994f44d072ab19c2027a1
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-09-03 15:22:19 +0530

    updating the file

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import useAuthStore from '../store/useAuthStore'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: 'https://leadms.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
